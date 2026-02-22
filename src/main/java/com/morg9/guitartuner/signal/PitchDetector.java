f9adbbfd908ddf3f206ecc13b057282b068ff9a2
Stephen Morgan (2024) ZNAC379 <morgan.stephen.2024@live.rhul.ac.uk>
2026-02-22 21:20:35 +0000

    #5 Implementing computeAutocorrelation()

--- src/main/java/com/morg9/guitartuner/signal/PitchDetector.java
+++ src/main/java/com/morg9/guitartuner/signal/PitchDetector.java
@@ -22,13 +22,40 @@ public class PitchDetector {
 	}
 	
 	private double[] computeAutocorrelation(double[] samples) {
-		// TODO
-		return null;
+		int sampleCount = samples.length;
+		double[] autocorrelation = new double[sampleCount];
+		int maxLag = (int) (sampleRate / minFrequency);
+		
+		for (int lag = 0; lag <= maxLag && lag < sampleCount; lag++) {
+			double correlationSum = 0.0;
+			double originalEnergy = 0.0;
+			double shiftedEnergy = 0.0;
+			
+			for (int i = 0; i < sampleCount - lag; i++) {
+				double originalSample = samples[i];
+				double shiftedSample = samples[i + lag];
+				
+				correlationSum += originalSample * shiftedSample;
+				
+				originalEnergy += originalSample * originalSample;
+				shiftedEnergy += shiftedSample * shiftedSample;
+			}
+			
+			if (originalEnergy > 0 && shiftedEnergy > 0) {
+				autocorrelation[lag] = correlationSum / Math.sqrt(originalEnergy * shiftedEnergy);
+			} else {
+				autocorrelation[lag] = 0.0;
+			}
+		}
+		return autocorrelation;
 	}
 	
 	private int findBestLag(double[] autocorrelation) {
+		
 		int minLag = (int) (sampleRate / maxFrequency);
 		int maxLag = (int) (sampleRate / minFrequency);
+		
+		double threshold = 0.3;
 		
 		double maxValue = Double.NEGATIVE_INFINITY;
 		int bestLag = -1;
@@ -39,6 +66,11 @@ public class PitchDetector {
 				bestLag = lag;
 			}
 		}
+		
+		if (maxValue < threshold ) {
+			return -1;
+		}
+		
 		return bestLag;
 	}
 }
